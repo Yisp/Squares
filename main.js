54941dde0f7a4a7fae78441854a120a4ff0b183a
Yisp <youjustdoxxedme@gmail.com>
2017-06-30 13:43:07 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,73 @@
+function Square() {
+
+	this.r = random(0, 255);
+	this.g = random(0, 255);
+	this.b =random(0, 255);
+
+	this.x = mouseX;
+	this.y = mouseY;
+
+	this.yspeed = random(-10, 10);
+	this.xspeed = random(-10, 10);
+
+	this.w = random(25, 50);
+	this.h = random(25, 50);
+
+	this.show = function() {
+
+		rectMode(CENTER);
+
+		fill(this.r, this.g, this.b);
+		rect(this.x, this.y, this.w, this.w);
+		noFill();
+
+	}
+
+	this.move = function() {
+
+		this.x += this.xspeed;
+		this.y += this.yspeed;
+
+	}
+
+}
+
+var squares = [];
+
+function setup() {
+
+	frameRate(60);
+	createCanvas(1360, 768);
+	background(0);
+}
+
+function draw() {
+
+	background(0);
+
+	if(mouseIsPressed) {
+
+		squares.push(new Square());
+
+	}
+
+	for (var i = 0; i < squares.length; i++) {
+
+		if (squares[i].y <= 0 || squares[i].x >= width ||
+			squares[i].y >= height || squares[i].x <= 0) {
+
+			squares.splice(i, 1);
+			break;
+
+		}
+
+	}
+
+	for (var i = 0; i < squares.length; i++) {
+
+		squares[i].move();
+		squares[i].show();
+
+	}
+
+}
